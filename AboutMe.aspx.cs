87532ed53c129543ede891d0631f64102d38e12a
Kymani Patterson <38448086+Kmanix@users.noreply.github.com>
2024-01-04 14:15:02 -0500

    Add files via upload

---
+++ AboutMe.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication1.BackEnd;
+
+namespace WebApplication1.Pages
+{
+    public partial class AboutMe : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Email mail = new Email();
+            mail.sendEmail("kymanipattersonfw@gmail.com", "kymanipattersonfw@gmail.com", TextBox1.Text,"kymanipattersonfw@gmail.com", "ecygihsqucztwcgi");
+        }
+    }
+}
